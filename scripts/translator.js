c9632c5ed8b187141f662459f573a683a5a9b9e7
Anwar Debes <anward@stud.ntnu.no>
2024-08-27 15:40:43 +0200

    translator.js strcture + adding a drop down menu

---
+++ scripts/translator.js
@@ -0,0 +1,13 @@
+// if the dropdown is changed, call the getTranslatedText function based 
+// on the selected language and display the translated text
+function getTranslatedText() {
+    const url = '';
+}
+function displayTranslatedInfo (){    
+}
+function translatorErrorHandler (){
+    
+}
+// https://apertium.org/apy/translate?langpair=en|es&q=Hello+world
+// this is the endpoint we are going to use to translate the text
+
